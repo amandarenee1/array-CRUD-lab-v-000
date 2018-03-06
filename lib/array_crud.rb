e9300fca4fca79c0123fb829c2bfdd5d16d5c4e5
Amanda Wingate <arw0219@gmail.com>
2018-03-06 01:29:31 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  animals = ["wow", "dog", "cat", "rabbit", "goat", "arrays!"]
+  animals = ["wow", "dog", "am", "rabbit", "goat", "arrays!"]
   animals [2]
 end
 
